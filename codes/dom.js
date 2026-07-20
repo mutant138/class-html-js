ac8a48329eb8fabd50fc2bf2c2ba437e33b0aab8
mutant138 <udhayasurya138@gmail.com>
2026-07-20 21:34:46 +0530

    feat: implement DOM manipulation script and restructure UI with new styles and layout

---
+++ codes/dom.js
@@ -0,0 +1,15 @@
+// console.dir(document)
+// console.log(document.title)
+// console.log(document.doctype)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[9])
+// document.all[9].textContent= "Nazith"
+
+
+// console.log(document)
+
+console.log(document.getElementById("header-title"))
+var header = document.getElementById("header-title")
+
+header.textContent = "Nazith"
