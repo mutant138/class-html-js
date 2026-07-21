0446b355cdbda1cbf08bb7ffc901b21c9915a815
mutant138 <udhayasurya138@gmail.com>
2026-07-21 21:27:08 +0530

    feat: implement DOM element creation and insertion logic while refining list structure in index.html

--- codes/dom.js
+++ codes/dom.js
@@ -9,7 +9,42 @@
 
 // console.log(document)
 
-console.log(document.getElementById("header-title"))
-var header = document.getElementById("header-title")
+// console.log(document.getElementById("header-title"))
+// var header = document.getElementById("header-title")
 
-header.textContent = "Nazith"
+// header.textContent = "Nazith"
+
+var listItems = document.getElementById("items")
+
+// console.log(listItems.parentElement.parentElement)
+
+
+// console.log(listItems.previousElementSibling)
+// console.log(listItems.childNodes)
+
+// console.log(listItems.children)
+
+// listItems.children[2].style.backgroundColor = "blue"
+
+// console.log(listItems.lastChild)
+
+// console.log(listItems.lastElementChild)
+
+// listItems.lastElementChild.textContent = "Partha"
+// listItems.firstElementChild.textContent = "Nazith"
+
+var newDiv = document.createElement("div")
+
+newDiv.textContent = "Sanjith"
+
+// newDiv.setAttribute("id", "sabari")
+
+newDiv.id = "sabari"
+
+newDiv.className = "Ganesh"
+
+var container = document.querySelector(".container")
+var header = document.querySelector("#header-title")
+container.insertBefore(newDiv, header )
+
+console.log(newDiv)
